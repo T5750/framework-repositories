ef72514070459495ad5d84a729f57f3909a4ebb5
evangel_z <evangel_a@sina.com>
2018-12-01 08:50:54 +0800

    added WebSocket in netty.

---
+++ socket/netty/src/main/java/t5750/netty/util/HttpHeaderUtil.java
@@ -0,0 +1,40 @@
+package t5750.netty.util;
+
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpMessage;
+import io.netty.handler.codec.http.HttpVersion;
+
+/**
+ * Copied from netty-all 5.0.0.Alpha2.
+ */
+public final class HttpHeaderUtil {
+	/**
+	 * Returns {@code true} if and only if the connection can remain open and
+	 * thus 'kept alive'. This methods respects the value of the
+	 * {@code "Connection"} header first and then the return value of
+	 * {@link HttpVersion#isKeepAliveDefault()}.
+	 */
+	public static boolean isKeepAlive(HttpMessage message) {
+		CharSequence connection = message.headers()
+				.get(HttpHeaderNames.CONNECTION);
+		if (connection != null && HttpHeaderValues.CLOSE.equals(connection)) {
+			return false;
+		}
+		if (message.protocolVersion().isKeepAliveDefault()) {
+			return !HttpHeaderValues.CLOSE.equals(connection);
+		} else {
+			return HttpHeaderValues.KEEP_ALIVE.equals(connection);
+		}
+	}
+
+	/**
+	 * Sets the {@code "Content-Length"} header.
+	 */
+	public static void setContentLength(HttpMessage message, int length) {
+		message.headers().setInt(HttpHeaderNames.CONTENT_LENGTH, length);
+	}
+
+	private HttpHeaderUtil() {
+	}
+}
---
+++ socket/netty/src/main/java/t5750/netty/websocket/WebSocketServer.java
@@ -0,0 +1,55 @@
+package t5750.netty.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import t5750.netty.util.NettyUtil;
+
+public class WebSocketServer {
+	public void run(int port) throws Exception {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						protected void initChannel(SocketChannel ch)
+								throws Exception {
+							ChannelPipeline pipeline = ch.pipeline();
+							pipeline.addLast("http-codec",
+									new HttpServerCodec());
+							pipeline.addLast("aggregator",
+									new HttpObjectAggregator(65536));
+							ch.pipeline().addLast("http-chunked",
+									new ChunkedWriteHandler());
+							pipeline.addLast("handler",
+									new WebSocketServerHandler());
+						}
+					});
+			Channel ch = b.bind(port).sync().channel();
+			System.out
+					.println("Web socket server started at port " + port + '.');
+			System.out.println(
+					"Open your browser and navigate to http://localhost:" + port
+							+ '/');
+			ch.closeFuture().sync();
+		} finally {
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		new WebSocketServer().run(NettyUtil.PORT_8765);
+	}
+}
