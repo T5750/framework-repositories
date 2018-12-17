c581eb086d996c6585c3e55958782babee9619f7
T5750 <evangel_a@sina.com>
2018-12-17 15:03:05 +0800

    updated Storm.

---
+++ storm/src/main/java/t5750/storm/drpc/basic/BasicDRPCTopology.java
@@ -0,0 +1,59 @@
+package t5750.storm.drpc.basic;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.LocalDRPC;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.drpc.LinearDRPCTopologyBuilder;
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+/**
+ * This topology is a basic example of doing distributed RPC on top of Storm. It
+ * implements a function that appends a "!" to any string you send the DRPC
+ * function.
+ * <p/>
+ * See https://github.com/nathanmarz/storm/wiki/Distributed-RPC for more
+ * information on doing distributed RPC on top of Storm.
+ */
+public class BasicDRPCTopology {
+	public static class ExclaimBolt extends BaseBasicBolt {
+		@Override
+		public void execute(Tuple tuple, BasicOutputCollector collector) {
+			String input = tuple.getString(1);
+			collector.emit(new Values(tuple.getValue(0), input + "!"));
+		}
+
+		@Override
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
+			declarer.declare(new Fields("id", "result"));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		LinearDRPCTopologyBuilder builder = new LinearDRPCTopologyBuilder(
+				"exclamation");
+		builder.addBolt(new ExclaimBolt(), 3);
+		Config conf = new Config();
+		if (args == null || args.length == 0) {
+			LocalDRPC drpc = new LocalDRPC();
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology("drpc-demo", conf,
+					builder.createLocalTopology(drpc));
+			for (String word : new String[] { "hello", "goodbye" }) {
+				System.out.println("Result for \"" + word + "\": "
+						+ drpc.execute("exclamation", word));
+			}
+			cluster.shutdown();
+			drpc.shutdown();
+		} else {
+			conf.setNumWorkers(3);
+			StormSubmitter.submitTopology(args[0], conf,
+					builder.createRemoteTopology());
+		}
+	}
+}
--- storm/src/main/java/t5750/storm/util/StormUtil.java
+++ storm/src/main/java/t5750/storm/util/StormUtil.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 public class StormUtil {
 	public static final String WINDOWS_FILE_DIR = "D:\\code\\";
+	public static final String INET_HOST = "192.168.100.163";
 
 	public static void hasDirectory() {
 		File file = new File(WINDOWS_FILE_DIR);
