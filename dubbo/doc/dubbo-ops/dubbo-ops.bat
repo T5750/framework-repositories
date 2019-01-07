efaffe21672785a1ba7b8a27cc8913435a204c5c
T5750 <evangel_a@sina.com>
2019-01-07 09:20:47 +0800

    added dubbo-ops.bat.

---
+++ dubbo/doc/dubbo-ops/dubbo-ops.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM ------------------------------------------------------------------------
+REM https://github.com/T5750/framework-repositories/blob/master/dubbo/doc/DubboNote.md#3-dubbo-ops
+REM mvn clean package
+REM mvn --projects dubbo-admin-backend spring-boot:run
+REM ------------------------------------------------------------------------
+echo.
+echo.
+echo             Choose the command
+echo 1-----------------run-------------------
+echo 2-------------clean and run-------------
+set /p num=Choose:
+if"%num%"=="1" (
+mvn --projects dubbo-admin-backend spring-boot:run
+)
+if"%num%"=="2" (
+mvn clean package
+mvn --projects dubbo-admin-backend spring-boot:run
+)
+REM :exit
