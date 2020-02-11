94a811e7e8b9fc97b687ce9a4e0e9a13f59f7131
evangel_z <evangel_a@sina.com>
2020-02-11 17:09:55 +0800

    Update spring-boot-sample for wx

---
+++ spring-boot/spring-boot-sample/src/main/java/t5750/springboot/module/wx/model/WxSession.java
@@ -0,0 +1,22 @@
+package t5750.springboot.module.wx.model;
+
+public class WxSession {
+	private String session_key;
+	private String openid;
+
+	public String getSession_key() {
+		return session_key;
+	}
+
+	public void setSession_key(String session_key) {
+		this.session_key = session_key;
+	}
+
+	public String getOpenid() {
+		return openid;
+	}
+
+	public void setOpenid(String openid) {
+		this.openid = openid;
+	}
+}
